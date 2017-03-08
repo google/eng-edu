1527c776e7d5f873fc3ca2f6e0528345fb0b8a0e
Pavel Simakov <psimakov@users.noreply.github.com>
2017-03-08 08:48:50 -0800

    Compute Datalab VM zone automatically for connect and delete operations.

--- ml/cc/bin/common.sh
+++ ml/cc/bin/common.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-MLCC_ZONE=us-central1-a
+MLCC_ZONE=""
 MLCC_USER_NAME=$(echo "${USER}" | sed 's/[^a-zA-Z0-9]//g')
 MLCC_INSTANCE=mlccvm-${MLCC_USER_NAME}
 
@@ -41,6 +41,16 @@ function assert_datalab_vm_does_not_exist() {
   fi
 }
 
+function resolve_zone() {
+  MLCC_ZONE=$(gcloud compute instances list --limit 1 \
+      --filter "name:$MLCC_INSTANCE" --format 'value(zone)')
+  if [[ -z "$MLCC_ZONE" ]]; then
+    echo "ERROR: Unable to find zone for a Datalab VM instance $MLCC_INSTANCE"
+    exit 1
+  fi
+  echo "Found Datalab VM ${MLCC_INSTANCE} in zone $MLCC_ZONE"
+}
+
 function assert_datalab_vm_exists() {
   echo "Checking Datalab VM ${MLCC_INSTANCE} exists"
   INFO=$(gcloud compute instances list --format='value(name)')
@@ -55,6 +65,7 @@ function assert_datalab_vm_exists() {
   else
     true
   fi
+  resolve_zone
 }
 
 function assert_datalab_vm_running() {
