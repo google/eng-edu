a0702d001d93483c416d8d67e21dcc0434bde53c
Ye Zheng <csyezheng@gmail.com>
2020-06-10 01:18:49 +0800

    Fix encoding error when reading files under windows

--- ml/guides/text_classification/load_data.py
+++ ml/guides/text_classification/load_data.py
@@ -48,7 +48,7 @@ def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
         train_path = os.path.join(imdb_data_path, 'train', category)
         for fname in sorted(os.listdir(train_path)):
             if fname.endswith('.txt'):
-                with open(os.path.join(train_path, fname)) as f:
+                with open(os.path.join(train_path, fname), encoding='utf-8') as f:
                     train_texts.append(f.read())
                 train_labels.append(0 if category == 'neg' else 1)
 
@@ -59,7 +59,7 @@ def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
         test_path = os.path.join(imdb_data_path, 'test', category)
         for fname in sorted(os.listdir(test_path)):
             if fname.endswith('.txt'):
-                with open(os.path.join(test_path, fname)) as f:
+                with open(os.path.join(test_path, fname), encoding='utf-8') as f:
                     test_texts.append(f.read())
                 test_labels.append(0 if category == 'neg' else 1)
 
